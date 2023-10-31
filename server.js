5ebfd3a97576782538a8751e155b44997f6d795c
Gen Vikat <gen@veebisepad.ee>
2023-10-31 15:57:33 +0200

    added dotenv package and configured in server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const PORT = env(PORT) || 3000;
+const PORT = process.env.PORT || 3000;
 const prisma = new PrismaClient();
 
 app.use(express.json());
